0c325eb4a832e539aca1ac2d53fe0b5df345e314
Marisol Madrigal Monroy <marisolmadrigalmonroy@outlook.com>
2026-02-28 22:09:54 -0600

    Comentarios en español para comoponente Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,10 @@
 import { useState, useEffect } from 'react'
-import api from '../api'
-import Project from '../components/Project'
 import HomeShowcase from '../components/HomeShowcase'
 
+/*
+* Componente que representa la p´gina de bienvenida 
+*/
 function Home() {
-    const [projects, setProjects] = useState([])
-    const [name, setName] = useState('')
-    const [description, setDescription] = useState('')
-    const [mentor, setMentor] = useState()
-    const [students, setStudents] = useState([])
-    const [categories, setCategories] = useState([])
-    const [requiredAbilities, setRequiredAbilities] = useState([])
-
     return <HomeShowcase />
 }
 
