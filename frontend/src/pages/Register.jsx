767952146ee8b96213d8e31885a8c06076fddaaa
Marisol Madrigal Monroy <marisolmadrigalmonroy@outlook.com>
2026-03-01 14:04:06 -0600

    Comentarios en español para Register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import Form from '../components/Form'
 
+/*
+* Componente que representa la página de registro
+*/
 function Register() {
     return <Form route='/api/user/register/' method='register'/>
 }
