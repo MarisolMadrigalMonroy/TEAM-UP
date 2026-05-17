c737baa1dcf1a244fe446342f1f6360958fb505c
Marisol Madrigal Monroy <marisolmadrigalmonroy@outlook.com>
2026-05-17 05:36:22 -0600

    variables de entorno

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=o7$^6!pc#futzudp^sd&3*22@*8cd04!#1%ccod1uwei$au+@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
